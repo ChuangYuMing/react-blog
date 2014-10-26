a8009463d7808048e4c81cc470d0f3c0cc22cf4d
ChuangYuMing <harry1123@hotmail.com.tw>
2014-10-26 10:28:35 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var react = require('gulp-react');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
+
+
+gulp.task('jsx', function () {
+    return gulp.src('javascript/**/*.js')
+        .pipe(plumber({
+          errorHandler: onError
+        }))
+        .pipe(react())
+        .pipe(gulp.dest('dist'));
+});
+
+// Watch files
+gulp.task('watch', function() {
+  gulp.watch('*.html', ['bs-reload']);
+  gulp.watch('javascript/*.js', ['jsx', 'bs-reload']);
+});
+
+// Error handeler
+var onError = function (err) {  
+    console.log('//////////////////////////////');
+    gutil.log(gutil.colors.yellow(err.message));
+    console.log('//////////////////////////////');
+    //gutil.beep();
+    browserSync.notify(err.message, 5000);
+};
+
+// Reload all Browsers
+gulp.task('bs-reload', ['jsx'], function () {
+    browserSync.reload();
+});
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: './'
+    },
+    port: 8080,
+    watchOptions: {debounceDelay: 1000}
+  })
+});
+
+
+gulp.task('default',['jsx', 'watch', 'browser-sync']);
